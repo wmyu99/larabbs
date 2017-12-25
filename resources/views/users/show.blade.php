1075b242c757d76231ec73848d7485bdef7faacb
vagrant <vagrant@homestead>
2017-12-25 10:11:08 +0000

    User profile page

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('title',$user->name.'的个人中心')
+@section('content')
+<div class="row">
+
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="media">
+                    <div align="center">
+                        <img class="thumbnail img-responsive" src="#">
+                    </div>
+                    <div class="media-body">
+                        <hr>
+                        <h4><strong>个人简介</strong></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <hr>
+                        <h4><strong>注册于</strong></h4>
+                        <p>January 01 1901</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg col-md-9 col-sm-12 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <span>
+                    <h1 class="panel panel-title pull-left" style="font-size:30px;">{{$user->name}}
+                </span>
+            </div>
+
+            {{--用户发布的内容--}}
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    暂无数据~~
+                </div>
+            </div>
+            @stop
