f53de3e12ea8f3e0cb6dbab8032cc48c84374ec5
vagrant <vagrant@homestead>
2018-01-29 10:06:44 +0000

    User can upload avatar

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -32,12 +32,16 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="botton" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px;margin-top=-5px;">
-                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="{{Auth::user()->avatar}}" class="img-responsive img-circle" width="30px" height="30px">
                             </span>
                             {{Auth::user()->name}} <span class="center"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+                           <li>
+                               <a href="{{route('users.edit',Auth::id())}}">
+                                   编辑资料
+                               </a>
                             <li>
                                 <a href="{{route('logout') }}"
                                 onclick="event.preventDefault();document.getElementById('logout-form').submit();">
